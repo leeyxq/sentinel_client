f451662d1831bf4406c91e1eaa8a7072dbf263c3
李向前 <lixq@fun.tv>
2019-07-26 18:13:18 +0800

    添加httpclient配置

--- src/main/java/com/lixq/springdemo/config/FeignConfig.java
+++ src/main/java/com/lixq/springdemo/config/FeignConfig.java
@@ -1,10 +1,16 @@
 package com.lixq.springdemo.config;
 
+import feign.RequestInterceptor;
+import feign.Retryer;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+
+import java.util.Map;
 
 /**
  * Feign 自定义配置
@@ -12,11 +18,30 @@ import org.springframework.context.annotation.Import;
  * @author lixiangqian
  * @date 2019-07-26 11:17
  **/
-@Import(FeignAutoConfiguration.class)
-@Configuration
+//@Import(FeignAutoConfiguration.class)
+@Configuration()
+@ConfigurationProperties(prefix = "feign.httpclient")
 public class FeignConfig {
 
     @Setter
     @Getter
-    private String userAgent;
+    private Map<String, String> headers;
+
+    @Bean
+    public Retryer feignRetry() {
+        //return new Retryer.Default(100, SECONDS.toMillis(1), 3);
+        return Retryer.NEVER_RETRY;
+    }
+
+//    @Bean
+//    public RequestInterceptor basicAuthRequestInterceptor() {
+    //认证用
+//        return new BasicAuthRequestInterceptor("root", "12345", Charsets.UTF_8);
+//    }
+
+
+    @Bean
+    public RequestInterceptor baseCommonHeaderRequestInterceptor() {
+        return template -> headers.entrySet().forEach(entry -> template.header(entry.getKey(), entry.getValue()));
+    }
 }
